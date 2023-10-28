2d5be76e5a74fb173a2db3d8e68100365e2218d4
Lucas Gasenzer <lucasgasenzer@mac.com>
2023-10-28 16:32:48 +0200

    add exit code doctumentation

--- templates.js
+++ templates.js
@@ -3,10 +3,25 @@ module.exports = {
         `&#9654;&#65039; <b>${e.Actor.Attributes.name}</b> started\n${e.Actor.Attributes.image}`,
 
     container_die: e => {
-        if (e.Actor.Attributes.exitCode === "1") {
-            return `&#128308; <b>${e.Actor.Attributes.name}</b> stopped!\n${e.Actor.Attributes.image}\nExit Code: ${e.Actor.Attributes.exitCode}`;
+        const exitCode = e.Actor.Attributes.exitCode;
+        const exitCodeMap = {
+            "0": "Success: Container exited with code 0",
+            "1": "General error: Container exited with code 1",
+            "2": "Misuse of shell builtins: Container exited with code 2",
+            "126": "Command invoked cannot execute: Container exited with code 126",
+            "127": "Command not found: Container exited with code 127",
+            "128": "Invalid argument to exit: Container exited with code 128",
+            "130": "Container terminated by user: Container exited with code 130",
+            "137": "Container received a SIGKILL: Container exited with code 137",
+            "139": "Segmentation fault: Container exited with code 139",
+            "143": "Container received a SIGTERM: Container exited with code 143",
+            // Add more exit codes as needed
+        };
+
+        if (exitCode in exitCodeMap) {
+            return `&#128308; <b>${e.Actor.Attributes.name}</b> stopped!\n${e.Actor.Attributes.image}\n${exitCodeMap[exitCode]}`;
         } else {
-            return `&#128308; <b>${e.Actor.Attributes.name}</b> stopped with a non-standard exit code!\n${e.Actor.Attributes.image}\nExit Code: ${e.Actor.Attributes.exitCode}`;
+            return `&#128308; <b>${e.Actor.Attributes.name}</b> stopped with an unknown exit code (${exitCode})!\n${e.Actor.Attributes.image}`;
         }
     },
 
@@ -16,3 +31,4 @@ module.exports = {
     'container_health_status: unhealthy': e =>
         `&#x26A0; <b>${e.Actor.Attributes.name}</b> unhealthy!\n${e.Actor.Attributes.image}`,
 };
+
