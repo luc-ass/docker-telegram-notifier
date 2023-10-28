d0bb13e4191fc3d574d19bbe8a39b7350b22927d
Lucas Gasenzer <lucasgasenzer@mac.com>
2023-10-28 16:26:48 +0200

    split container_die by exitCode

--- templates.js
+++ templates.js
@@ -2,8 +2,13 @@ module.exports = {
     container_start: e =>
         `&#9654;&#65039; <b>${e.Actor.Attributes.name}</b> started\n${e.Actor.Attributes.image}`,
 
-    container_die: e =>
-        `&#128308; <b>${e.Actor.Attributes.name}</b> stopped!\n${e.Actor.Attributes.image}\nExit Code: ${e.Actor.Attributes.exitCode}`,
+    container_die: e => {
+        if (e.Actor.Attributes.exitCode === "1") {
+            return `&#128308; <b>${e.Actor.Attributes.name}</b> stopped!\n${e.Actor.Attributes.image}\nExit Code: ${e.Actor.Attributes.exitCode}`;
+        } else {
+            return `&#128308; <b>${e.Actor.Attributes.name}</b> stopped with a non-standard exit code!\n${e.Actor.Attributes.image}\nExit Code: ${e.Actor.Attributes.exitCode}`;
+        }
+    },
 
     'container_health_status: healthy': e =>
         `&#9989; <b>${e.Actor.Attributes.name}</b> healthy\n${e.Actor.Attributes.image}`,
